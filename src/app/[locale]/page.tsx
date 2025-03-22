4bc25da4d9f607274f6ba7f1cd740db959dd5ef0
sobrepoxidev <sobrepoxidev@gmail.com>
2025-03-22 10:41:19 -0600

    what

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
         <GridImage />
         <FlightSearch />
         <Footer />
-        {/* <WhatsAppButton /> */}
+        <WhatsAppButton />
       </div>
     </main>
   );
