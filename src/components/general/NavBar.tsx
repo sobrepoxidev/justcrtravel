7818ffc0aabd5a70717c0f02029d6d964e5861cc
sobrepoxidev <sobrepoxidev@gmail.com>
2025-03-23 17:17:40 -0600

    si

--- src/components/general/NavBar.tsx
+++ src/components/general/NavBar.tsx
@@ -3,14 +3,19 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { FaHome } from "react-icons/fa";
 import { useTranslations } from 'next-intl';
-import ToursDropdown from './ToursDropdow';
-import ServicesDropdown from './ServicesDropdown';
+import dynamic from 'next/dynamic';
 import LocaleSwitcher from '../locale/LocaleSwitcher';
+
+// Dynamically import client components with no SSR
+const ToursDropdown = dynamic(() => import('./ToursDropdow'));
+const ServicesDropdown = dynamic(() => import('./ServicesDropdown'));
+const MobileMenu = dynamic(() => import('./MobileMenu'));
 
 const Navbar = () => {
   const t = useTranslations("navbar");
+  
   return (
-    <nav className="absolute w-full z-10 bg-black/45 border-b border-orange-400 ">
+    <nav className="absolute w-full z-10 bg-black/45 border-b border-orange-400">
       <div className="container mx-auto px-2 py-3 flex justify-between items-center">
         <Link href="/" className="flex items-center">
           <Image 
@@ -22,59 +27,57 @@ const Navbar = () => {
           />
         </Link>
         
+        {/* Desktop Navigation */}
         <div className="hidden md:flex space-x-1">
           <Link href="/" className="text-white hover:text-orange-300 px-3 py-2 flex items-center gap-x-1">
             <FaHome />{t("home")}
           </Link>
           <ToursDropdown />
           <Link href="/en/destinations" className="text-white hover:text-orange-300 px-3 py-2">
-          {t("destinations")}
+            {t("destinations")}
           </Link>
           <ServicesDropdown />
           <Link href="/en/about-us" className="text-white hover:text-orange-300 px-3 py-2">
-          {t("about")}
+            {t("about")}
           </Link>
           <Link href="/en/payments" className="text-white hover:text-orange-300 px-3 py-2">
-          {t("payments")}
+            {t("payments")}
           </Link>
           <Link href="/en/contact" className="text-white hover:text-orange-300 px-3 py-2">
-          {t("contact")}
+            {t("contact")}
           </Link>
           <LocaleSwitcher />
         </div>
         
-        <div className="flex items-center space-x-4">
-        
-          {/* <Link href="/en" className="text-white hover:text-orange-300">
-            🇺🇸 EN
+        {/* Social Media Icons - Desktop */}
+        <div className="hidden md:flex items-center space-x-2 text-white">
+          <Link href="/search" aria-label="Search">
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
           </Link>
-          <Link href="/es" className="text-white hover:text-orange-300">
-            🇪🇸 ES_ES
-          </Link> */}
-          <div className="hidden md:flex items-center space-x-2 text-white">
-            <Link href="/search" aria-label="Search">
-              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-            </Link>
-            <Link href="/facebook" aria-label="Facebook">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96C15.9 21.58 18.04 20.39 19.63 18.64C21.22 16.89 22.17 14.66 22.17 12.36C22.17 6.83 17.67 2.34 12.17 2.34L12 2.04Z" />
-              </svg>
-            </Link>
-            <Link href="/instagram" aria-label="Instagram">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 2.16C15.2 2.16 15.58 2.18 16.88 2.24C18.06 2.3 18.76 2.5 19.2 2.68C19.8 2.9 20.24 3.18 20.68 3.62C21.12 4.06 21.4 4.5 21.62 5.1C21.8 5.54 22 6.24 22.06 7.42C22.12 8.72 22.14 9.1 22.14 12.3C22.14 15.5 22.12 15.88 22.06 17.18C22 18.36 21.8 19.06 21.62 19.5C21.4 20.1 21.12 20.54 20.68 20.98C20.24 21.42 19.8 21.7 19.2 21.92C18.76 22.1 18.06 22.3 16.88 22.36C15.58 22.42 15.2 22.44 12 22.44C8.8 22.44 8.42 22.42 7.12 22.36C5.94 22.3 5.24 22.1 4.8 21.92C4.2 21.7 3.76 21.42 3.32 20.98C2.88 20.54 2.6 20.1 2.38 19.5C2.2 19.06 2 18.36 1.94 17.18C1.88 15.88 1.86 15.5 1.86 12.3C1.86 9.1 1.88 8.72 1.94 7.42C2 6.24 2.2 5.54 2.38 5.1C2.6 4.5 2.88 4.06 3.32 3.62C3.76 3.18 4.2 2.9 4.8 2.68C5.24 2.5 5.94 2.3 7.12 2.24C8.42 2.18 8.8 2.16 12 2.16ZM12 0C8.74 0 8.33 0.02 7.0 0.08C5.68 0.14 4.82 0.35 4.06 0.65C3.28 0.97 2.62 1.38 1.96 2.04C1.3 2.7 0.89 3.36 0.57 4.14C0.27 4.9 0.06 5.76 0 7.08C-0.06 8.42 -0.08 8.82 -0.08 12.08C-0.08 15.34 -0.06 15.74 0 17.08C0.06 18.4 0.27 19.26 0.57 20.02C0.89 20.8 1.3 21.46 1.96 22.12C2.62 22.78 3.28 23.19 4.06 23.51C4.82 23.81 5.68 24.02 7.0 24.08C8.33 24.14 8.74 24.16 12 24.16C15.26 24.16 15.67 24.14 17 24.08C18.32 24.02 19.18 23.81 19.94 23.51C20.72 23.19 21.38 22.78 22.04 22.12C22.7 21.46 23.11 20.8 23.43 20.02C23.73 19.26 23.94 18.4 24 17.08C24.06 15.74 24.08 15.34 24.08 12.08C24.08 8.82 24.06 8.42 24 7.08C23.94 5.76 23.73 4.9 23.43 4.14C23.11 3.36 22.7 2.7 22.04 2.04C21.38 1.38 20.72 0.97 19.94 0.65C19.18 0.35 18.32 0.14 17 0.08C15.67 0.02 15.26 0 12 0Z" />
-                <path d="M12 5.84C8.6 5.84 5.84 8.6 5.84 12C5.84 15.4 8.6 18.16 12 18.16C15.4 18.16 18.16 15.4 18.16 12C18.16 8.6 15.4 5.84 12 5.84ZM12 16C9.79 16 8 14.21 8 12C8 9.79 9.79 8 12 8C14.21 8 16 9.79 16 12C16 14.21 14.21 16 12 16Z" />
-                <path d="M18.4 5.6C18.4 6.37 17.77 7 17 7C16.23 7 15.6 6.37 15.6 5.6C15.6 4.83 16.23 4.2 17 4.2C17.77 4.2 18.4 4.83 18.4 5.6Z" />
-              </svg>
-            </Link>
-            <Link href="/twitter" aria-label="Twitter">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M22.46 6C21.69 6.35 20.86 6.58 20 6.69C20.88 6.16 21.56 5.32 21.88 4.31C21.05 4.81 20.13 5.16 19.16 5.36C18.37 4.5 17.26 4 16 4C13.65 4 11.73 5.92 11.73 8.29C11.73 8.63 11.77 8.96 11.84 9.27C8.28 9.09 5.11 7.38 3 4.79C2.63 5.42 2.42 6.16 2.42 6.94C2.42 8.43 3.17 9.75 4.33 10.5C3.62 10.5 2.96 10.3 2.38 10V10.03C2.38 12.11 3.86 13.85 5.82 14.24C5.19 14.41 4.53 14.44 4 14.31C4.5 16.03 6.02 17.26 7.89 17.29C6.37 18.45 4.61 19.13 2.65 19.13C2.3 19.13 1.95 19.11 1.61 19.07C3.5 20.29 5.78 21 8.06 21C16 21 20.33 14.46 20.33 8.79C20.33 8.6 20.33 8.42 20.32 8.23C21.16 7.63 21.88 6.87 22.46 6Z" />
-              </svg>
-            </Link>
-          </div>
+          <Link href="/facebook" aria-label="Facebook">
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96C15.9 21.58 18.04 20.39 19.63 18.64C21.22 16.89 22.17 14.66 22.17 12.36C22.17 6.83 17.67 2.34 12.17 2.34L12 2.04Z" />
+            </svg>
+          </Link>
+          <Link href="/instagram" aria-label="Instagram">
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 2.16C15.2 2.16 15.58 2.18 16.88 2.24C18.06 2.3 18.76 2.5 19.2 2.68C19.8 2.9 20.24 3.18 20.68 3.62C21.12 4.06 21.4 4.5 21.62 5.1C21.8 5.54 22 6.24 22.06 7.42C22.12 8.72 22.14 9.1 22.14 12.3C22.14 15.5 22.12 15.88 22.06 17.18C22 18.36 21.8 19.06 21.62 19.5C21.4 20.1 21.12 20.54 20.68 20.98C20.24 21.42 19.8 21.7 19.2 21.92C18.76 22.1 18.06 22.3 16.88 22.36C15.58 22.42 15.2 22.44 12 22.44C8.8 22.44 8.42 22.42 7.12 22.36C5.94 22.3 5.24 22.1 4.8 21.92C4.2 21.7 3.76 21.42 3.32 20.98C2.88 20.54 2.6 20.1 2.38 19.5C2.2 19.06 2 18.36 1.94 17.18C1.88 15.88 1.86 15.5 1.86 12.3C1.86 9.1 1.88 8.72 1.94 7.42C2 6.24 2.2 5.54 2.38 5.1C2.6 4.5 2.88 4.06 3.32 3.62C3.76 3.18 4.2 2.9 4.8 2.68C5.24 2.5 5.94 2.3 7.12 2.24C8.42 2.18 8.8 2.16 12 2.16ZM12 0C8.74 0 8.33 0.02 7.0 0.08C5.68 0.14 4.82 0.35 4.06 0.65C3.28 0.97 2.62 1.38 1.96 2.04C1.3 2.7 0.89 3.36 0.57 4.14C0.27 4.9 0.06 5.76 0 7.08C-0.06 8.42 -0.08 8.82 -0.08 12.08C-0.08 15.34 -0.06 15.74 0 17.08C0.06 18.4 0.27 19.26 0.57 20.02C0.89 20.8 1.3 21.46 1.96 22.12C2.62 22.78 3.28 23.19 4.06 23.51C4.82 23.81 5.68 24.02 7.0 24.08C8.33 24.14 8.74 24.16 12 24.16C15.26 24.16 15.67 24.14 17 24.08C18.32 24.02 19.18 23.81 19.94 23.51C20.72 23.19 21.38 22.78 22.04 22.12C22.7 21.46 23.11 20.8 23.43 20.02C23.73 19.26 23.94 18.4 24 17.08C24.06 15.74 24.08 15.34 24.08 12.08C24.08 8.82 24.06 8.42 24 7.08C23.94 5.76 23.73 4.9 23.43 4.14C23.11 3.36 22.7 2.7 22.04 2.04C21.38 1.38 20.72 0.97 19.94 0.65C19.18 0.35 18.32 0.14 17 0.08C15.67 0.02 15.26 0 12 0Z" />
+              <path d="M12 5.84C8.6 5.84 5.84 8.6 5.84 12C5.84 15.4 8.6 18.16 12 18.16C15.4 18.16 18.16 15.4 18.16 12C18.16 8.6 15.4 5.84 12 5.84ZM12 16C9.79 16 8 14.21 8 12C8 9.79 9.79 8 12 8C14.21 8 16 9.79 16 12C16 14.21 14.21 16 12 16Z" />
+              <path d="M18.4 5.6C18.4 6.37 17.77 7 17 7C16.23 7 15.6 6.37 15.6 5.6C15.6 4.83 16.23 4.2 17 4.2C17.77 4.2 18.4 4.83 18.4 5.6Z" />
+            </svg>
+          </Link>
+          <Link href="/twitter" aria-label="Twitter">
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M22.46 6C21.69 6.35 20.86 6.58 20 6.69C20.88 6.16 21.56 5.32 21.88 4.31C21.05 4.81 20.13 5.16 19.16 5.36C18.37 4.5 17.26 4 16 4C13.65 4 11.73 5.92 11.73 8.29C11.73 8.63 11.77 8.96 11.84 9.27C8.28 9.09 5.11 7.38 3 4.79C2.63 5.42 2.42 6.16 2.42 6.94C2.42 8.43 3.17 9.75 4.33 10.5C3.62 10.5 2.96 10.3 2.38 10V10.03C2.38 12.11 3.86 13.85 5.82 14.24C5.19 14.41 4.53 14.44 4 14.31C4.5 16.03 6.02 17.26 7.89 17.29C6.37 18.45 4.61 19.13 2.65 19.13C2.3 19.13 1.95 19.11 1.61 19.07C3.5 20.29 5.78 21 8.06 21C16 21 20.33 14.46 20.33 8.79C20.33 8.6 20.33 8.42 20.32 8.23C21.16 7.63 21.88 6.87 22.46 6Z" />
+            </svg>
+          </Link>
+        </div>
+        
+        {/* Mobile Menu Toggle Button */}
+        <div className="flex md:hidden">
+          <MobileMenu/>
         </div>
       </div>
     </nav>
