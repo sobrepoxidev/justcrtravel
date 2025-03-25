24532185852f5dd8ba3326326bbb5b0b46ac3a6c
sobrepoxidev <sobrepoxidev@gmail.com>
2025-03-25 11:55:02 -0600

    fa

--- src/components/general/MobileMenu.tsx
+++ src/components/general/MobileMenu.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from 'react';
 import Link from 'next/link';
-import { FaHome, FaSearch } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
 import LocaleSwitcher from '../locale/LocaleSwitcher';
 import { useTranslations } from 'next-intl';
 
