fa1ed74b5fcfc8bd61e91443378e6885e66e262c
sobrepoxidev <sobrepoxidev@gmail.com>
2025-03-27 10:14:25 -0600

    Analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 //locale
 import { NextIntlClientProvider, hasLocale } from 'next-intl';
@@ -44,6 +45,7 @@ export default async function RootLayout({
             {children}
             
             <SpeedInsights />
+            <Analytics />
         </NextIntlClientProvider>
       </body>
     </html>
