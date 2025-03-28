9d90a264167f0f83f8a2030098636e32a6329cb7
sobrepoxidev <sobrepoxidev@gmail.com>
2025-03-27 21:39:23 -0600

    aspect final

--- src/app/[locale]/cultural/page.tsx
+++ src/app/[locale]/cultural/page.tsx
@@ -7,28 +7,28 @@ import { useTranslations } from 'next-intl';
 export default function Cultural() {
     const t = useTranslations("cultural");
 
-    const culturalKeys = ["cities", "nightLife", "parks", "indigenous"];
+    const culturalKeys = ["nightLife", "cities", "parks", "indigenous"];
 
     const images = [
-        "/culturals/cultural-1.jpg",
         "/culturals/cultural-2.jpg",
+        "/culturals/cultural-1.jpg",
         "/culturals/cultural-3.jpg",
         "/culturals/cultural-4.jpg"
     ];
 
     return (
-        <div className="relative min-h-screen bg-gradient-to-br from-blue-50 to-white">
+        <div className="relative min-h-screen bg-gradient-to-br from-blue-50 to-white ">
             <Navbar />
-            <div className="max-w-6xl mx-auto px-4 pt-20 sm:pt-36 pb-0">
+            <div className="max-w-6xl mx-auto px-4 pt-20 sm:pt-36 pb-0 ">
                 <h1 className="text-3xl sm:text-5xl font-bold tracking-tight mb-4 text-gray-950">{t("title")}</h1>
 
                 <div className="w-full max-w-5xl mx-auto">
-                    <div  className="w-full h-7 sm:h-12 relative overflow-hidden">
+                <div className="w-full h-7 sm:h-12 relative overflow-hidden">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 1000 100"
                             preserveAspectRatio="none"
-                            className="absolute bottom-0 left-0 w-full h-full"
+                            className="absolute bottom-0 w-full h-full"
                         >
                             <path
                                 d="M0,6V0h1000v100L0,6z"
@@ -44,16 +44,19 @@ export default function Cultural() {
 
                         return (
                             <div key={key} className="relative max-w-full flex flex-col space-y-4">
-                                <div className={`flex flex-col md:flex-row items-start ${index % 2 === 1 ? "md:flex-row-reverse" : ""}`}>
-                                    <div className="max-w-full w-full md:w-1/2 pt-2 flex flex-col items-center ">
-                                    <div className="w-full" style={{ aspectRatio: "1.31" }}>
-                                        <Image
-                                            src={images[index]}
-                                            alt={t(`${key}.name`)}
-                                            width={600}
-                                            height={400}
-                                            className="w-full h-auto rounded shadow-m"
-                                        />
+                                <div
+                                    className={`flex flex-col md:flex-row items-start
+                                                ${index % 2 === 1 ? "md:flex-row-reverse" : ""}`}
+                                >
+                                <div className="max-w-full w-full md:w-1/2 pt-2 flex flex-col items-center ">
+                                <div className="w-full" style={{ aspectRatio: "1.31" }}>
+                                            <Image
+                                                src={images[index]}
+                                                alt={t(`${key}.name`)}
+                                                width={600}
+                                                height={400}
+                                                className="w-full h-auto rounded shadow-md"
+                                            />
                                         </div>
                                     </div>
                                     <div className="md:w-1/2 md:px-8">
