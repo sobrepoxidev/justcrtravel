0e82b4672ee12885e5486f33bebc9b4a20f10720
sobrepoxidev <sobrepoxidev@gmail.com>
2025-03-29 22:07:44 -0600

    q

--- src/i18n/routing.tsx
+++ src/i18n/routing.tsx
@@ -2,8 +2,8 @@ import {defineRouting} from 'next-intl/routing';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'es'],
+  locales: ['es', 'en'],
  
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'es'
 });
